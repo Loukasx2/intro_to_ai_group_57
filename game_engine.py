be486aa3453f3aad240febc2e79c518c6198b7f8
Javier Moreno <javier@meilirobots.com>
2024-03-07 17:42:20 +0100

    refact and better design

--- game_engine.py
+++ game_engine.py
@@ -5,12 +5,24 @@ import sys
 
 color_light = (202, 203, 213)
 color_dark = (2, 6, 145)
+y_scaling_factor = 1.5
 
+# Neon colors:
+class Colors:
+    # Modified tones of existing colors
+    RED = (255, 80, 80)       # Lighter red
+    YELLOW = (255, 255, 102)  # Lighter yellow
+    ORANGE = (255, 140, 0)    # Darker orange
+    GREEN = (0, 200, 0)       # Darker green
+    PURPLE = (178, 102, 255)  # Lighter purple
+    BLUE = (0, 102, 204)      # Darker blue
+    BLACK = (51, 51, 51)      # Modified black
+    GREY = (160, 160, 160)
+    WHITE = (240, 240, 240)   # Modified white
 
 def SixPlayers(number_of_players):
     pygame.init()
-    matrix = numpy.ones((17, 25))
-    matrix *= -1
+    matrix = numpy.ones((17, 25)) * -1
 
     if number_of_players == 2:
         players_list = [
@@ -39,7 +51,7 @@ def SixPlayers(number_of_players):
             [[4, 0], [4, 2], [4, 4], [4, 6], [5, 1], [5, 3], [5, 5], [6, 2], [6, 4], [7, 3]]
         ]
     else:
-        print("Invalid number of players. Please provide a valid integer.")
+        print("Invalid number of players. Valid options are 2, 3, 4, or 6.")
         sys.exit(1)
 
     move_index = [[-1, -1], [-1, 1], [0, 2], [1, 1], [1, -1], [0, -2]]
@@ -64,15 +76,15 @@ def SixPlayers(number_of_players):
             matrix[player[i][0]][player[i][1]] = index
         index += 1
 
-    def pion():
-        colors = ["grey", "red", "yellow", "orange", "green", "purple", "blue"]
+    def draw_pawns():
+        colors = [Colors.GREY, Colors.RED, Colors.YELLOW, Colors.ORANGE, Colors.GREEN, Colors.PURPLE, Colors.BLUE]
         for i in range(0, 17):
             for j in range(0, 25):
                 if matrix[i][j] >= 0:
                     rect = pygame.Rect(
-                        j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE
+                        j * CELL_SIZE, i * CELL_SIZE * y_scaling_factor, CELL_SIZE, CELL_SIZE
                     )
-                    pions_rect.append(
+                    pawns_rect.append(
                         pygame.draw.rect(
                             screen,
                             colors[int(matrix[i][j])],
@@ -81,7 +93,7 @@ def SixPlayers(number_of_players):
                         )
                     )
 
-    def valid_moves(coor):
+    def get_valid_moves(coor):
         valid_index = []
         for i in range(len(move_index)):
 
@@ -117,21 +129,21 @@ def SixPlayers(number_of_players):
     def get_token_coor(x, y):
         grid_width = 0
         grid_heigth = 0
-        coor = [int((y - grid_heigth) / 20), int((x - grid_width) / 20)]
+        coor = [int((y - grid_heigth) / (CELL_SIZE * y_scaling_factor)), int((x - grid_width) / CELL_SIZE)]
         return coor
 
-    def animation(moves=[], clicked_token=None):
-        colors = ["grey", "red", "yellow", "orange", "green", "purple", "blue"]
+    def add_selected_effect(moves=[], clicked_token=None):
+        colors = [Colors.GREY, Colors.RED, Colors.YELLOW, Colors.ORANGE, Colors.GREEN, Colors.PURPLE, Colors.BLUE]
         moves.append(clicked_token)
         for i in range(0, 17):
             for j in range(0, 25):
                 if matrix[i][j] >= 0:
                     rect = pygame.Rect(
-                        j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE
+                        j * CELL_SIZE, i * CELL_SIZE * y_scaling_factor, CELL_SIZE, CELL_SIZE
                     )
-                    pions_rect.append(
+                    pawns_rect.append(
                         pygame.draw.rect(
-                            screen, colors[int(matrix[i][j])], rect, border_radius=20
+                            screen, colors[int(matrix[i][j])], rect, border_radius=CELL_SIZE
                         )
                     )
                 if [i, j] in moves:
@@ -139,14 +151,14 @@ def SixPlayers(number_of_players):
                     test_cercle = pygame.transform.scale(
                         test_cercle, (CELL_SIZE, CELL_SIZE)
                     )
-                    screen.blit(test_cercle, (j * CELL_SIZE, i * CELL_SIZE))
+                    screen.blit(test_cercle, (j * CELL_SIZE, i * CELL_SIZE * y_scaling_factor))
 
-    def WriteText(text, text_pos_x, text_pos_y, text_size, col):
+    def write_text(text, text_pos_x, text_pos_y, text_size, col):
         text_font = pygame.font.SysFont(None, text_size)
         text_render = text_font.render(text, True, col)
         screen.blit(text_render, (text_pos_x, text_pos_y))
 
-    def winner():
+    def check_winner():
         #TODO: Implement the winner function
         return False
 
@@ -156,24 +168,24 @@ def SixPlayers(number_of_players):
     screen = pygame.display.set_mode(size=(num_columns * CELL_SIZE, num_rows * CELL_SIZE + 100))
     timer = pygame.time.Clock()
     game_on = True
-    pions_rect = []
+    pawns_rect = []
 
-    screen.fill(pygame.Color("white"))
-    pion()
+    screen.fill(pygame.Color(Colors.BLACK))
+    draw_pawns()
     player_index = 1
     player_valid_moves = []
     last_selected_token = []
 
-    player_colours = ["red", "yellow", "orange", "green", "purple", "blue"]
+    player_colours = [Colors.RED, Colors.YELLOW, Colors.ORANGE, Colors.GREEN, Colors.PURPLE, Colors.BLUE]
 
     while game_on:
         col = player_colours[player_index - 1]
         
-        if winner() == False:
-            WriteText(
+        if check_winner() == False:
+            write_text(
                 "Player " + str(player_index) + "'s Turn",
                 num_columns * CELL_SIZE - 370,
-                num_rows * CELL_SIZE - 100,
+                num_rows * CELL_SIZE + 30,
                 50,
                 col,
             )
@@ -187,7 +199,7 @@ def SixPlayers(number_of_players):
                 pos = pygame.mouse.get_pos()
 
                 # get a list of all sprites that are under the mouse cursor
-                clicked_sprites = [s for s in pions_rect if s.collidepoint(pos)]
+                clicked_sprites = [s for s in pawns_rect if s.collidepoint(pos)]
 
                 if clicked_sprites:
                     clicked_token = get_token_coor(
@@ -197,24 +209,21 @@ def SixPlayers(number_of_players):
                         if clicked_token == last_selected_token:
                             last_selected_token = []
                             player_valid_moves = []
-                            screen.fill(pygame.Color("white"))
-                            animation()
                         else:
-                            player_valid_moves = valid_moves(clicked_token)
+                            player_valid_moves = get_valid_moves(clicked_token)
                             last_selected_token = clicked_token
-                            screen.fill(pygame.Color("white"))
-                            animation(player_valid_moves, last_selected_token)
+                        screen.fill(pygame.Color(Colors.BLACK))
+                        add_selected_effect(player_valid_moves, last_selected_token)
                     elif clicked_token in player_valid_moves:
                         move(last_selected_token, clicked_token)
-                        winner()
+                        check_winner()
                         last_selected_token = []
                         player_valid_moves = []
-                        screen.fill(pygame.Color("white"))
+                        screen.fill(pygame.Color(Colors.BLACK))
                         player_index = (player_index + 1) % (number_of_players + 1)
                         if player_index == 0:
                             player_index += 1
-
-                        animation()
+                        add_selected_effect()
         pygame.display.update()
         timer.tick(60)
 
